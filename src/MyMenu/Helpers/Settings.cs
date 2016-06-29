8a5b7e582328b7042064c1f50b5c3cddb263c7a2
Nish <nish@xamarin.com>
2016-06-29 23:38:53 +0530

    Updated to Latest NuGets. No tests run 😕

--- src/MyMenu/Helpers/Settings.cs
+++ src/MyMenu/Helpers/Settings.cs
@@ -2,7 +2,7 @@
 using Plugin.Settings;
 using Plugin.Settings.Abstractions;
 
-namespace MyMenuAdmin.Helpers
+namespace MyMenu.Helpers
 {
   /// <summary>
   /// This is the Settings static class that can be used in your Core solution or in any
